eb99cb40531bd21a35054d35ea3324cae7cc72b2
Jemin Yeon <yjm9507@yonsei.ac.kr>
2021-05-26 16:53:49 +0900

    New app instagram with migration done

---
+++ django/instagram/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Post(models.Model):
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
